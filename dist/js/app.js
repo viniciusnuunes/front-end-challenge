532917153cfa73d7d4111b5c7c93b5596a26bc3f
Vinicius Nunes <viniiciuspereiira@hotmail.com>
2017-11-17 18:44:16 -0200

    Iniciando com API

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,10 +1,1 @@
 var app = angular.module("app", []);
-
-// $http({
-//  method: 'GET',
-//  url: '/url-da-api' })
-//  .then(function successCallback(response) {
-//    // se der tudo certo ele vai cair aqui
-//    }, function errorCallback(response) {
-//    // se der errrado vai cair aqui
-//  }); 
--- dist/js/controller.js
+++ dist/js/controller.js
@@ -1,3 +1,16 @@
-app.controller("MyController", function($scope){
+app.controller("MyController", function($scope, $http){
   $scope.titulo = "Teste";
+  $http.get('http://rest-service.guides.spring.io/greeting')
+  .then(function(response) {
+    $scope.greeting = response.data;
+  });
 });
+
+// $http({
+//  method: 'GET',
+//  url: '/url-da-api' })
+//  .then(function successCallback(response) {
+//    // se der tudo certo ele vai cair aqui
+//    }, function errorCallback(response) {
+//    // se der errrado vai cair aqui
+//  });
